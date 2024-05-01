32cf7cb92aefcd52368d93e369e73574b3644752
Женя <dtxtrump@gmail.com>
2024-05-01 19:24:25 +0300

    fin2

--- test_hw.py
+++ test_hw.py
@@ -92,24 +92,24 @@ def test_readable_function():
     find_registration_button_on_login_page(page_url="https://companyname.com/login", button_text="Register")
 
 
-def print_name(function, *args):
-    result_name = function.__name__.replace('_', ' ').title()
-    result_args = ', '.join(args)
-    res = f'{result_name} [{result_args}]'
+def name_func(function, *args):
+    res_name = function.__name__.replace('_', ' ').title()
+    res_args = ', '.join(args)
+    res = f'{res_name} [{res_args}]'
 
     return res
 
 
 def open_browser(browser_name):
-    actual_result = print_name(open_browser, browser_name)
+    actual_result = name_func(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result =  print_name(go_to_companyname_homepage, page_url)
+    actual_result = name_func(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = print_name(find_registration_button_on_login_page, page_url, button_text)
+    actual_result = name_func(find_registration_button_on_login_page, page_url, button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
